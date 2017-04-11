e73bede7ff3e55d874f28e76a67ea9d27f17b1f1
Robert Diggle <Robschach13@yahoo.com>
2017-04-11 19:55:43 -0400

    Commit 2

--- serverPractice.js
+++ serverPractice.js
@@ -3,37 +3,39 @@ var http = require("http");
 
 
 // This is where the server will be listening. LISTENER
-var PORT = 8080;
+var PORT1 = 7000;
+var PORT2 = 7500;
 
-function handleRequest(request, response)
+
+function handleRequestOne(request, response)
 {
 	//This will be triggered when the user goes to our port.
-// var kong = ["Gigan","Rodan","Godzilla","King Kong"];
 
-// for (var i = 0; i <= kong.length; i++)
-//  {
-// 	response.end("<p>" + kong[i])
-// };
 
-	response.end("IT WORKS!! Path hit: " +  request.url);
-
+	response.end("Ich lebe dich");
 }
 
-//Create our server
-var server = http.createServer(handleRequest);
+function handleRequestTwo(request, response)
+{
+	response.end("Du bist ein Schwein.");
+}
+
+
+//Create our servers
+var server = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
 
 //Start it up!
-server.listen(PORT, function()
+server.listen(PORT1, function()
 {
-console.log("Server is listening on port: " + PORT)
-
-
-
-
-
-
-
+console.log("Server is listening on port: " + PORT1);
 })
+
+serverTwo.listen(PORT2, function()
+{
+console.log("Server is listening on port: " + PORT2);
+})
+
 
 // Instructions
 // Using the previous example as a guide, create an app that has two web servers.
@@ -41,3 +43,11 @@ console.log("Server is listening on port: " + PORT)
 // The one listening on port 7000 will always tell the user something good about themselves.
 // The one listening on 7500 will always tell the user something bad about themselves.
 // Make sure you create a Github repo and commit this code!
+
+
+// var kong = ["Gigan","Rodan","Godzilla","King Kong"];
+
+// for (var i = 0; i <= kong.length; i++)
+//  {
+// 	response.end("<p>" + kong[i])
+// };
